5fa341ef3e6098263668c1bb7508b2a343803b94
sanyam <patilsanyam869@gmail.com>
2024-03-10 19:06:30 +0530

    added more controlles

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,7 +3,6 @@ import Student from '../models/student.model.js'
 import Schedule from '../models/schedule.model.js'
 
 const fetchAllStudent = expressAsyncHandler(async (req, res) => {
- 
   console.log('ujhbduhduhu')
 
   const student = await Student.find({})
@@ -17,26 +16,33 @@ const fetchAllStudent = expressAsyncHandler(async (req, res) => {
 const CreateScheLiveClass = expressAsyncHandler(async (req, res) => {
   // const {id} = req.body
   // console.log("id",id)
-const {Createby,TitleOfClass,Topic_heading,create_Date,Time} = req.body
-const schedule = await Schedule.create({
-  TitleOfClass,
-  Topic_heading,
-  create_Date,
-  Time,
-  Createby
-  // adminId,  
-
+  const { Createby, TitleOfClass, Topic_heading, create_Date, Time } = req.body
+  console.log(Createby,TitleOfClass,Topic_heading,create_Date,Time)
+  const schedule = await Schedule.create({
+    TitleOfClass,
+    Topic_heading,
+    create_Date,
+    Time,
+    Createby
+    // adminId,
+  })
+  res.status(200).json({
+    sucess: true,
+    schedule
+  })
+   await schedule.save()
+  // const schedule = await find({adminId})
 })
+
+const fetchAllSchedule = expressAsyncHandler(async (req, res) => {
+  console.log("allo under")
+  
+const allschedule = await Schedule.find({})
 res.status(200).json({
   sucess: true,
-  schedule  
-})
-// const schedule = await find({adminId})
-
-
-
+  massege:"gya dabun",
+  allschedule
 })
 
-
-export { fetchAllStudent,CreateScheLiveClass}
-
+})
+export { fetchAllStudent, CreateScheLiveClass ,fetchAllSchedule}
--- src/router/dashborad.route.js
+++ src/router/dashborad.route.js
@@ -1,10 +1,13 @@
  import { Router } from 'express'
  import { AdminfetchAllDoubts } from '../controllers/Doubt.controller.js'
- import {fetchAllStudent,CreateScheLiveClass} from '../controllers/dashboard.controller.js'
+ import {fetchAllStudent,CreateScheLiveClass, fetchAllSchedule} from '../controllers/dashboard.controller.js'
  const dashboardRouter = Router()
  dashboardRouter.route('/student-doubts').get(AdminfetchAllDoubts)
  dashboardRouter.route('/all-student').get(fetchAllStudent)
  dashboardRouter.route('/create-schedule').post(CreateScheLiveClass)
+ dashboardRouter.route('/all-schedule').get(fetchAllSchedule)
+
+
 
 
  export default dashboardRouter
