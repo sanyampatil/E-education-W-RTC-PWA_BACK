e2ccc7a1bde7f6babf8175f62b010251b2373db2
sanyam patil <patilsanyam869@gmail.com>
2025-01-02 18:57:03 +0530

    Update dbconnection.js

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 // import { DB_NAME } from "../constants.js";
-
+import { config } from 'dotenv'
+config()
 const connectDB = async () => {
   try {
     // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)`
